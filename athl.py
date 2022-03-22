2eff87c6bbebc158929f8de1b7ed3a19f3568602
Marco António Sousa <smarqito@gmail.com>
2022-03-22 00:02:50 +0000

    index.html restructure

--- athl.py
+++ athl.py
@@ -6,21 +6,36 @@ reg = r'(?P<id>\w+),(?P<index>\d+),(?P<date>\d{4}-\d{2}-\d{2}),(?P<primeiro>\w+)
 
 f = open("assets/emd.csv")
 
+def get_key(match):
+    gd = match.groupdict()
+    return gd["primeiro"] + " " + gd["ultimo"]
+
 def reader():
+    todos = []
     inde = open("index.html", "w")
     inde.write('<ul>')
     for l in f.readlines():
-        templ = open("template/athlete.html")
-        templat = templ.read()
         m = match(reg, l)
         if m:
-            gd = m.groupdict()
-            inde.write(f'<li><a href="athlete/{gd["id"]}.html">{gd["primeiro"]}, {gd["ultimo"]}</a></li>')
-            for k in gd.keys():
-                templat = sub(rf'{{{{{k}}}}}', gd[k], templat)
-            nathl = open(f'athlete/{gd["id"]}.html', 'w')
-            nathl.write(templat)
-        templ.close()
+            todos.append(m)
+    sys.stdin
+    ## alinha
+    todos.sort(key=get_key)
+    
+    templ = open("template/athlete.html")
+    templat = templ.read()
+    
+    for m in todos:
+        atleta = templat
+        gd = m.groupdict()
+        
+        inde.write(f'<li><a href="athlete/{gd["id"]}.html">{gd["primeiro"]}, {gd["ultimo"]}</a></li>')
+        for k in gd.keys():
+            atleta = sub(rf'{{{{{k}}}}}', gd[k], atleta)
+
+        nathl = open(f'athlete/{gd["id"]}.html', 'w')
+        nathl.write(atleta)
+    templ.close()
     inde.write('</ul>')
     inde.close()
 
