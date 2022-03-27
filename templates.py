4ed178ac05c784f1b91ad499ff364a3312d3117a
Marco António Sousa <smarqito@gmail.com>
2022-03-27 15:00:05 +0100

    template fixed

--- templates.py
+++ templates.py
@@ -15,7 +15,8 @@ import sys
 from multipledispatch import dispatch
 #import os
 
-def load_templates(rootPath : str, temps: dict) -> dict:
+
+def load_templates(rootPath: str, temps: dict) -> dict:
     '''load templates to a dictionary, using root dir'''
     for t_key, t_name in temps.items():
         t = open(f'{rootPath}{t_name}', 'r')
@@ -23,16 +24,19 @@ def load_templates(rootPath : str, temps: dict) -> dict:
         t.close()
     return temps
 
+
 @dispatch(dict, str)
 def template(obj: dict, temp: str) -> str:
     '''Utilizar em templates sem listas:\n
     \t obj[key] must match with template {{key}}
     \tWill replace {{key}} w/ obj[key] value
     '''
-    print("template dict:", obj)
-    for k in obj:
+
+    subs = findall(r'\{\{(\w+)\}\}', temp)
+    for k in subs:
         temp = sub(rf'{{{{{k}}}}}', str(obj[k]), temp)
     return temp
+
 
 @dispatch(list, str)
 def template(obj: list, temp: str) -> str:
@@ -44,35 +48,77 @@ def template(obj: list, temp: str) -> str:
             res += sub(r'\{\{\w+\}\}', str(e), temp)
     return res
 
-@dispatch(object, str, dict)
-def template(obj, temp, temps: dict) -> str:
-    '''Utilizar em templates com listas\n
-        temps -> {'template_name': 'template'}\n
-        Estrategia:\n
-        \tIf obj[key] is a list, then populate over the template in temps\n
-        \t\ttemp should have {{temp_name, key}} to be overriden
-        \tIf is not a list, behaves the same way as simple template
-    '''
+
+@dispatch(object, str)
+def template(obj, temp: str) -> str:
+    if type(obj) is dict:
+        return template(obj, temp)
+    else:
+        return sub(r'\{\{\w+\}\}', str(obj), temp)
+
+
+@dispatch(dict, str, dict)
+def template(obj: dict, temp: str, temps: dict) -> str:
     res = temps[temp]
-    print("template:", temp)
-    iterable = findall(r'\{\{(\w+), (\w+)\}\}', res)
-    
-    for t, key in iterable:
-        if type(obj) is dict:
-            print("antes dict")
-            found = template(obj[key], t, temps) # itera todos os que tem templates
-            print("depois dict")
-            res = sub(rf'{{{{{t}, {key}}}}}', found, res) # preenche a template
-        elif type(obj) is list:
-            found = ''
-            for e in obj:
-                if type(e) is dict:
-                    found += template(e[key], t, temps)
-                else:
-                    found += template(e, t, temps)
-            res = sub(rf'{{{{{t}, {key}}}}}', found, temps[temp])
+    arrays = findall(r'\{\{(\w+), (\w+)\}\}', res)
+    for tmp, key in arrays:
+        nt = template(obj[key], tmp, temps)
+        res = sub(rf'{{{{{tmp}, {key}}}}}', nt, res)
 
     res = template(obj, res)
-
     return res
 
+
+@dispatch(list, str, dict)
+def template(o_list, temp, temps) -> str:
+    res = temps[temp]
+    arrays = findall(r'\{\{(\w+), (\w+)\}\}', res)
+    v = ''
+    if arrays:
+        for tmp, key in arrays:
+            for e in o_list:
+                if type(e) is dict or list:
+                    v += template(e, temp, temps)
+                else:
+                    v += template(e, temps[tmp])
+    else:
+        for e in o_list:
+            v += template(e, temp, temps)
+    #res = template(o_list, res)
+    return v
+
+@dispatch(object, str, dict)
+def template(obj, temp, temps):
+    return template(obj, temps[temp])
+
+# @dispatch(object, str, dict)
+# def template(obj, temp, temps: dict) -> str:
+#     '''Utilizar em templates com listas\n
+#         temps -> {'template_name': 'template'}\n
+#         Estrategia:\n
+#         \tIf obj[key] is a list, then populate over the template in temps\n
+#         \t\ttemp should have {{temp_name, key}} to be overriden
+#         \tIf is not a list, behaves the same way as simple template
+#     '''
+#     res = temps[temp]
+#     print("template:", temp)
+#     iterable = findall(r'\{\{(\w+), (\w+)\}\}', res)
+
+#     for t, key in iterable:
+#         if type(obj) is dict:
+#             print("antes dict")
+#             found = template(obj[key], t, temps) # itera todos os que tem templates
+#             print("depois dict")
+#             res = sub(rf'{{{{{t}, {key}}}}}', found, res) # preenche a template
+#         elif type(obj) is list:
+#             found = ''
+#             for e in obj:
+#                 if type(e) is dict:
+#                     found += template(e[key], t, temps)
+#                 else:
+#                     found += template(e, t, temps)
+#             res = sub(rf'{{{{{t}, {key}}}}}', found, temps[temp])
+
+#     res = template(obj, res)
+
+#     return res
